e598ea64e7b6dcfb4f75982e2c98c73c660f37a5
ms <manfred@ms123.org>
2017-08-29 11:35:46 +0200

    timer and job handling now working

--- src/main/java/org/camunda/bpm/engine/impl/db/orientdb/handler/MessageEntityHandler.java
+++ src/main/java/org/camunda/bpm/engine/impl/db/orientdb/handler/MessageEntityHandler.java
@@ -14,4 +14,19 @@ public class MessageEntityHandler extends BaseEntityHandler{
 	public MessageEntityHandler(OrientGraph g) {
 		super( g, MessageEntity.class);
 	}
+	@Override
+	protected void setSuperClasses(OSchema schema, OClass oClass, boolean restricted) {
+		List<OClass> superList = new ArrayList<OClass>();
+		superList.add(schema.getClass("JobEntity"));
+		oClass.setSuperClasses(superList);
+	}
+
+	@Override
+	public void insertAdditional(OrientGraph orientGraph, Vertex v, Object entity, Class entityClass, Map<String, Vertex> entityCache) {
+		v.setProperty( "type", "MessageEntity");
+	}
+	@Override
+	protected OProperty getOrCreateProperty(OClass oClass, String propertyName, OType oType) {
+		return null;
+	}
 }
