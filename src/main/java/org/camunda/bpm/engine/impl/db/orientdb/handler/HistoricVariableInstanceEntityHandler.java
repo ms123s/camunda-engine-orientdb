0e389fb1af64522a2ad44187c49a238c5cba5287
ms <manfred@ms123.org>
2018-03-02 00:26:56 +0100

    dito

--- src/main/java/org/camunda/bpm/engine/impl/db/orientdb/handler/HistoricVariableInstanceEntityHandler.java
+++ src/main/java/org/camunda/bpm/engine/impl/db/orientdb/handler/HistoricVariableInstanceEntityHandler.java
@@ -7,6 +7,13 @@ import com.tinkerpop.blueprints.impls.orient.OrientGraph;
 import com.tinkerpop.blueprints.Vertex;
 import java.util.List;
 import java.util.Map;
+import java.util.Collection;
+import java.util.ArrayList;
+import com.github.raymanrt.orientqb.query.Clause;
+import static com.github.raymanrt.orientqb.query.Clause.and;
+import static com.github.raymanrt.orientqb.query.Clause.clause;
+import static com.github.raymanrt.orientqb.query.Clause.or;
+import static com.github.raymanrt.orientqb.query.Operator.EQ;
 
 /**
  * @author Manfred Sattler
@@ -16,6 +23,18 @@ public class HistoricVariableInstanceEntityHandler extends BaseEntityHandler{
 
 	public HistoricVariableInstanceEntityHandler(OrientGraph g) {
 		super( g, HistoricVariableInstanceEntity.class);
+	}
+	@Override
+	public void addToClauseList(List<Clause> clauseList, String statement, Object parameter, Map<String, Object> queryParams) {
+		String[] taskIds = getValueByField(parameter, "taskIds");
+		log.info("HistoricVariableInstanceEntity.taskIds:"+taskIds);
+		if (taskIds != null && taskIds.length > 0) {
+			List<Clause> orList = new ArrayList<Clause>();
+			for (String taskId : taskIds) {
+				orList.add(clause("taskId", EQ, taskId));
+			}
+			clauseList.add(or(orList.toArray(new Clause[orList.size()])));
+		}
 	}
 	@Override
 	public void insertAdditional(Vertex v, Object entity, Map<Object, List<Vertex>> entityCache) {
