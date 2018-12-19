13faf6a513ea1ce45a8d49b844a02714383bb8de
ms <manfred@ms123.org>
2018-12-19 15:23:02 +0000

    new test server

--- src/main/java/org/simpl4/OrientDB.java
+++ src/main/java/org/simpl4/OrientDB.java
@@ -8,17 +8,17 @@ import com.orientechnologies.orient.core.db.OrientDBConfig;
 public class OrientDB {
 	private static Logger LOG = Logger.getLogger(OrientDB.class.getName());
 
-	private com.orientechnologies.orient.core.db.OrientDB orientDB;
+	private static com.orientechnologies.orient.core.db.OrientDB orientDB;
 	private static String rootPassword = "simpl4";
 
 	public synchronized ODatabasePool getDatabasePool(String db,String username, String pw ) {
 		if( orientDB == null){
-			orientDB = new com.orientechnologies.orient.core.db.OrientDB("remote:192.168.2.155", "root", rootPassword, OrientDBConfig.defaultConfig());
+			orientDB = new com.orientechnologies.orient.core.db.OrientDB("remote:192.168.2.243", "root", rootPassword, OrientDBConfig.defaultConfig());
 		}
 		if (!orientDB.exists(db)) {
 			orientDB.create(db,ODatabaseType.PLOCAL);
 		}
-
+		System.err.println("getDatabasePool("+username+","+pw);
 	  ODatabasePool pool = new ODatabasePool(orientDB, db, username, pw);
 		return pool;
 	}
